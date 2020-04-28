2b5d9b17097faf0d4bc37dc3de482622da232808
coldmanck <coldmanck@gmail.com>
2020-04-29 00:22:37 +0800

    revise 0022,0102

--- 0022_Generate_Parentheses.py
+++ 0022_Generate_Parentheses.py
@@ -3,30 +3,34 @@
 
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        # ans_set = set()
-        # def gen_par(i, left_arr, right_arr):
-        #     if i == 1:
-        #         ans_set.add(left_arr + '()' + right_arr)
+        '''Time = Space complexity = n-th Catalan number = 1/(n+1) * C^{2n}_n'''
+
+        '''Recursion'''
+        # def backtrack(ans, cur_ans='', n_left=0, n_right=0):
+        #     if len(cur_ans) == 2 * n:
+        #         ans.append(cur_ans)
         #         return
-        #     # '(' + gen_par(i-1) + ')'
-        #     gen_par(i-1, '(' + left_arr, right_arr + ')') 
-        #     # gen_par(i-1) + '()'
-        #     gen_par(i-1, left_arr, right_arr + '()')
-        #     # '()' + gen_par(i-1) 
-        #     gen_par(i-1, '()' + left_arr, right_arr)
-        #     # gen_par(i-2) + gen_par(i-2)
-        #     gen_par(i-2, '(' + left_arr + ')', '(' + right_arr + ')')
-        # gen_par(n, '', '')
-        # return list(ans_set)
-        ans = []
-        def backtrack(s, left, right):
-            if len(s) == 2*n:
-                ans.append(s)
-                return
-            if left < n:
-                backtrack(s + '(', left + 1, right)
-            if right < left:
-                backtrack(s + ')', left, right + 1)
+        #     if n_left < n:
+        #         backtrack(ans, cur_ans + '(', n_left + 1, n_right)
+        #     if n_right < n_left:
+        #         backtrack(ans, cur_ans + ')', n_left, n_right + 1)
         
-        backtrack('', 0, 0)
+        # ans = []
+        # backtrack(ans)
+        # return ans
+
+        '''Iterative solution (DFS)'''
+        if n == 0:
+            return []
+        stack = [('(', 1, 0)]
+        ans = []
+        while stack:
+            cur_ans, n_left, n_right = stack.pop()
+            if len(cur_ans) == 2 * n:
+                ans.append(cur_ans)
+                continue
+            if n_left < n:
+                stack.append((cur_ans + '(', n_left + 1, n_right))
+            if n_right < n_left:
+                stack.append((cur_ans + ')', n_left, n_right + 1))
         return ans
--- 0102_Binary_Tree_Level_Order_Traversal.py
+++ 0102_Binary_Tree_Level_Order_Traversal.py
@@ -1,16 +1,31 @@
-# Runtime: 36 ms, faster than 38.12% of Python3 online submissions for Binary Tree Level Order Traversal.
-# Memory Usage: 13.1 MB, less than 100.00% of Python3 online submissions for Binary Tree Level Order Traversal.
-
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+'''
+Time complexity : O(N) since each node is processed exactly once.
+Space complexity : O(N) to keep the output structure which contains N node values.
+'''
 
 from collections import deque
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        '''Recursive solution'''
+        def level_order(level, ans):
+            if len(level) == 0:
+                return
+            ans.append([node.val for node in level])
+            level_order([leaf for node in level for leaf in (node.left, node.right) if leaf], ans)
+        
+        if not root:
+            return []
+        ans = []
+        level_order([root], ans)
+        return ans
+        
         '''BFS with queue method'''
         # ans = []
         # temp_ans = []
@@ -33,11 +48,12 @@ class Solution:
         # if temp_ans:
         #     ans.append(temp_ans)
         # return ans
+        
         '''Method 2'''
-        if not root:
-            return []
-        ans, level = [], [root]
-        while level:
-            ans.append([node.val for node in level])
-            level = [leaf for node in level for leaf in (node.left, node.right) if leaf]
-        return ans
+        # if not root:
+        #     return []
+        # ans, level = [], [root]
+        # while level:
+        #     ans.append([node.val for node in level])
+        #     level = [leaf for node in level for leaf in (node.left, node.right) if leaf]
+        # return ans
