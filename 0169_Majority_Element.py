d66b992286ae8f02699a7e48b5f392f0eee19bf0
coldmanck <coldmanck@gmail.com>
2020-05-07 14:22:59 +0800

    revise 0169

--- 0169_Majority_Element.py
+++ 0169_Majority_Element.py
@@ -4,8 +4,22 @@
 from collections import defaultdict
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        counter = defaultdict(int)
-        for num in nums:
-            counter[num] += 1
-            if counter[num] > (len(nums) // 2):
-                return num
+        '''Sorting time O(nlogn) space O(1)'''
+        if len(nums) == 1:
+            return nums[0]
+        count = 1
+        nums.sort()
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                count += 1
+            else:
+                count = 1
+            if count > len(nums) // 2:
+                return nums[i]
+        
+        '''hashmap time O(n) space O(1)'''
+        # counter = defaultdict(int)
+        # for num in nums:
+        #     counter[num] += 1
+        #     if counter[num] > (len(nums) // 2):
+        #         return num
