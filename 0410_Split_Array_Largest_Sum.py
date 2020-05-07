5161c0ec81cff2e8a75628cc84fd3c7dcb02ba4b
coldmanck <coldmanck@gmail.com>
2020-05-07 12:36:17 +0800

    add 0410

---
+++ 0410_Split_Array_Largest_Sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def splitArray(self, nums: List[int], m: int) -> int:
+        # Time O(nlogn)
+        def is_valid(mid):
+            cur_sum = count = 0
+            for num in nums:
+                cur_sum += num
+                if cur_sum > mid:
+                    cur_sum = num
+                    count += 1
+                    if count == m:
+                        return False
+            return True
+        
+        left, right = max(nums), sum(nums)
+        while left < right:
+            mid = (left + right) // 2
+            if not is_valid(mid):
+                left = mid + 1
+            else:
+                right = mid
+        return left
