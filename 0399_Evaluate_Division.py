60bab68af1f9e76128408d2c2a53adf5131b6f08
coldmanck <coldmanck@gmail.com>
2020-05-18 00:05:11 +0800

    add 0399

---
+++ 0399_Evaluate_Division.py
@@ -0,0 +1,15 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        # floyd-warshall algorithm
+        # Time O(V^3) Space O(V^2)
+        from collections import defaultdict
+        graph = defaultdict(dict)
+        for (num, den), val in zip(equations, values):
+            graph[num][den] = val
+            graph[den][num] = 1 / val
+            graph[num][num] = graph[den][den] = 1.0
+        for k in graph:
+            for i in graph[k]:
+                for j in graph[k]:
+                    graph[i][j] = graph[i][k] * graph[k][j]
+        return [graph[num].get(den, -1.0) for num, den in queries]
