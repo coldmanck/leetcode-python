1e8dbea366af10c1440dfcbd3dc49ef240d1f663
coldmanck <coldmanck@gmail.com>
2020-05-29 14:36:26 +0800

    revise 0127

--- 0127_Word_Ladder.py
+++ 0127_Word_Ladder.py
@@ -1,13 +1,17 @@
 from collections import deque, defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        '''Time: O(m^2*n) Space: O(m^2*n)'''
         if endWord not in wordList:
             return 0
-        wordList = list(set(wordList) | set([beginWord]))
+        if beginWord not in wordList:
+            wordList.append(beginWord)
+        
         graph = defaultdict(list)
-        for word in wordList:
-            for i in range(len(word)):
-                graph[word[:i] + '*' + word[i + 1:]].append(word)
+        for word in wordList: # m times
+            for i in range(len(word)): # n times
+                graph[word[:i] + '*' + word[i + 1:]].append(word) # This takes O(m)!!
+        
         queue = deque()
         queue.append((beginWord, 1))
         visited = set([beginWord])
