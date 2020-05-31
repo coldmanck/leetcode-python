c4bc1abd224b92ceff2cf5d1f1b00f10b115af70
coldmanck <coldmanck@gmail.com>
2020-05-31 17:05:23 +0800

    revise 0069,0072

--- 0069_Sqrt(x).py
+++ 0069_Sqrt(x).py
@@ -1,16 +1,25 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        # time O(logn) space O(1)
+        '''time O(logn) space O(1)'''
+        # if x <= 1:
+        #     return x
+        # left, right = 1, x // 2
+        # while left <= right:
+        #     mid = left + (right - left) // 2
+        #     mid_square = mid ** 2
+        #     if mid_square == x or (mid_square < x and (mid_square + 2 * mid + 1) > x):
+        #         return mid
+        #     if mid_square < x:
+        #         left = mid + 1
+        #     else:
+        #         right = mid - 1
+        
+        '''Newton's Method'''
         if x <= 1:
             return x
-        
-        left, right = 1, x // 2
-        while left <= right:
-            mid = left + (right - left) // 2
-            mid_square = mid ** 2
-            if mid_square == x or (mid_square < x and (mid_square + 2 * mid + 1) > x):
-                return mid
-            if mid_square < x:
-                left = mid + 1
-            else:
-                right = mid - 1
+        guess = x // 2
+        while True:
+            guess2 = (guess + x / guess) / 2
+            if int(guess) == int(guess2):
+                return int(guess)
+            guess = guess2
--- 0072_Edit_Distance.py
+++ 0072_Edit_Distance.py
@@ -6,15 +6,32 @@ class Solution:
                                                                A[0:a-1], B[0:b-2]), # add B[b-1] to A
                                                                A[0:a-2], B[0:b-1])) # delete A[a-1]
         '''
-        A, B = word1, word2
-        cache = [[-1 for _ in range(len(B) + 1)] for _ in range(len(A) + 1)]
-        cache[0] = [i for i in range(len(B) + 1)]
-        for i in range(1, len(A) + 1):
-            for j in range(len(B) + 1):
+        '''DP with 2D cache: O(mn) time & space'''
+        # dp = [[i for i in range(len(word2) + 1)] for _ in range(len(word1) + 1)]
+        # for i in range(1, len(word1) + 1):
+        #     for j in range(len(word2) + 1):
+        #         if j == 0:
+        #             dp[i][j] = i
+        #         elif word1[i - 1] == word2[j - 1]:
+        #             dp[i][j] = dp[i - 1][j - 1]
+        #         else:
+        #             dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+        # return dp[-1][-1]
+        
+        '''DP with 1D cache: O(mn) time, O(min(m, n)) space'''
+        if len(word2) > len(word1):
+            word1, word2 = word2, word1
+        dp = [i for i in range(len(word2) + 1)]
+        for i in range(1, len(word1) + 1):
+            for j in range(len(word2) + 1):
                 if j == 0:
-                    cache[i][j] = i
-                elif A[i - 1] == B[j - 1]:
-                    cache[i][j] = cache[i - 1][j - 1]
+                    dp_j_minus_1 = dp[j]
+                    dp[j] = i
+                    continue
+                temp = dp[j]
+                if word1[i - 1] == word2[j - 1]:
+                    dp[j] = dp_j_minus_1
                 else:
-                    cache[i][j] = 1 + min(cache[i - 1][j - 1], cache[i - 1][j], cache[i][j - 1])
-        return cache[-1][-1]
+                    dp[j] = 1 + min(dp[j], dp[j - 1], dp_j_minus_1)
+                dp_j_minus_1 = temp
+        return dp[-1]
